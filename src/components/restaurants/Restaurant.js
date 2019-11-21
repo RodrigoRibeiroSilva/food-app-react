fb577bb30ca834871d56591a420c476ba7353dfd
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-11-20 23:33:32 -0300

    Exibindo restaurantes retornados pela API

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import ImgRestaurant from '../../assets/img/restaurants/tasty.png'
 
 class Restaurant extends React.Component {
 
     render(){
-        return (          
+        return (
+              
             <div className="col-sm-6 col-xs-12">
                   <a href="/restaurant">
                     <div className="place-info-box">
-                      <span className="place-info-box-icon"><img src={ImgRestaurant} alt="Tasty"/></span>
+                      <span className="place-info-box-icon"><img src={this.props.image} alt=""/></span>
 
                       <div className="place-info-box-content">
-                        <span className="place-info-box-text">Tasty Treats</span>
-                        <span className="place-info-box-star"><i class="fa fa-star"></i> 4.5</span>
-                        <span className="place-info-box-detail">Bakery</span>
-                        <span className="place-info-box-detail">40-65m</span>
+                        <span className="place-info-box-text">{this.props.name}</span>
+                        <span className="place-info-box-star"><i class="fa fa-star"></i> {this.props.rating} </span>
+                        <span className="place-info-box-detail">{this.props.category}</span>
+                        <span className="place-info-box-detail">{this.props.deliveryEstimate}</span>
                       </div>
                     </div>
                   </a>               
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { FOOD_API } from '../../api/Api'
+import Restaurant from './Restaurant';
 
 class Restaurants extends React.Component {
 
@@ -36,26 +37,47 @@ class Restaurants extends React.Component {
             }
         )
     }
+
+    renderRestaurants() {
+        let result = []
+        this.state.restaurants.map((restaurant) => {
+            result.push(<Restaurant
+                            image={restaurant.imagePath} 
+                            name={restaurant.name} 
+                            rating={restaurant.rating} 
+                            category={restaurant.category} 
+                            deliveryEstimate={restaurant.deliveryEstimate} >
+                        </Restaurant>)
+        })
+
+        return result
+    }
     
     render(){
-        const { error, isLoaded, restaurants } = this.state;
+        const { error, isLoaded } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
         }else if (!isLoaded) {
             return <div>Loading...</div>;
         } else{
-            return (            
-                <ul>
-                {restaurants.map(item => (
-                    <li key={item.name}>
-                    {item.name} 
-                    </li>
-                ))}
-            </ul>
-            );
+            return (
+            <div>          
+                <section className="content-header">
+                    <h1>
+                        Todos os Restaurantes
+                    </h1>
+                </section>
+                
+                <section className="content">
+                    <div className="row">
+                        {this.renderRestaurants()}
+                    </div>
+                </section>
+            </div>                        
+            );  
         }
-    }
-      
+    }  
 }
+
 
 export default Restaurants
